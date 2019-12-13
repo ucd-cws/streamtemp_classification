218ac1c9c1a722486840b8368f451d8ceec175d1
Ann Willis <awillis@ucdavis.edu>
2019-12-13 15:04:52 -0800

    Created new code and the objective description

---
+++ code/06_temperature_data_review_and_cleaning.R
@@ -0,0 +1,4 @@
+
+# Code description --------------------------------------------------------
+
+#This code is where we review and clean all the temperature data that we downloaded from CDEC and USGS.
