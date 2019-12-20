d1bf048cd27861a6c2b9fc856fafa625489ecf6c
Ann Willis <awillis@ucdavis.edu>
2019-12-19 16:36:03 -0800

    Test.

--- code/06_temperature_data_review_and_cleaning.R
+++ code/06_temperature_data_review_and_cleaning.R
@@ -1,4 +1,4 @@
 
 # Code description --------------------------------------------------------
 
-#This code is where we review and clean all the temperature data that we downloaded from CDEC and USGS.
+#This code is where we review and clean all the temperature data that we downloaded from CDEC and USGS. We'll get here eventually.
