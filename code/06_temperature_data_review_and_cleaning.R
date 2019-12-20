3d6da980027b58e2040294e2cabd1c28093a0cff
Ryan Peek <rapeek@ucdavis.edu>
2019-12-19 17:56:35 -0800

    add data_review rds files

--- code/06_temperature_data_review_and_cleaning.R
+++
@@ -1,4 +0,0 @@
-
-# Code description --------------------------------------------------------
-
-#This code is where we review and clean all the temperature data that we downloaded from CDEC and USGS. We'll get here eventually.
