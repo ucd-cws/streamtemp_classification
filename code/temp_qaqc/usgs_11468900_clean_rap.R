20cb23f1b877f31eae16ccacaf70d54a6a738a7a
Ryan Peek <rapeek@ucdavis.edu>
2020-01-07 14:29:55 -0800

    add plotly code

--- code/temp_qaqc/usgs_11468900_clean_rap.R
+++ code/temp_qaqc/usgs_11468900_clean_rap.R
@@ -17,4 +17,8 @@ usgs_minute_11468900 <- read_rds(path = paste0("data/data_review/",file_list[[95
 
 # Plot --------------------------------------------------------------------
 
+library(plotly)
 
+# now make an interactive plot of first 1000 values
+ggplotly(
+  ggplot() + geom_point(data=usgs_minute_11468900[1:1000,], aes(x=dateTime, y=X_00010_00000)))
