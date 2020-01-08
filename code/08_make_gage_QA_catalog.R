abdfceeb4f7dd5ae71303caec99987931eef74eb
Ann Willis <awillis@ucdavis.edu>
2020-01-08 10:44:24 -0800

    Saved the final code to make the gage QA catalog.

--- code/08_make_gage_QA_catalog.R
+++ code/08_make_gage_QA_catalog.R
@@ -8,4 +8,8 @@ load("data/all_gages.rda")
 all_gages %>% st_drop_geometry() -> all_gages
 
 gage_QA_progress <- all_gages %>% 
-  select()
+  select(site_id, site_name, operator)
+
+gage_QA_progress[c("reviewer", "completed(Y/N)", "notes")] <- NA
+
+write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
