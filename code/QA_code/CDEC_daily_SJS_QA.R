1c7e19a9d9f1a40bb1b5a57fcc01707c094a96d6
Ann Willis <awillis@ucdavis.edu>
2020-01-09 14:32:49 -0800

    Completed QA for cdec daily site SJS, saved code and data, updated gage_QA_progress table

--- code/QA_code/CDEC_daily_SJS_QA.R
+++ code/QA_code/CDEC_daily_SJS_QA.R
@@ -23,6 +23,8 @@ cdec_daily_SJS <- read_rds(path = paste0("data/data_review/",file_list[[6]]))
 ggplotly(
   ggplot() + geom_point(data=cdec_daily_SJS[1:1000,], aes(x=datetime, y=value)))
 
+# Review, QA, and Repeat --------------------------------------------------
+
 cdec_daily_SJS_QA <- cdec_daily_SJS[1:1000,]
 
 ggplotly(
@@ -60,10 +62,28 @@ ggplotly(
 
 cdec_daily_SJS_QA <- rbind(cdec_daily_SJS_QA, cdec_daily_SJS_3001_4000)
 
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_SJS[4001:5000,], aes(x=datetime, y=value)))
+
+cdec_daily_SJS_4001_5000 <- cdec_daily_SJS[4001:5000,] %>%
+  filter(value < 100, datetime != "2016-02-07")
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_SJS_4001_5000, aes(x=datetime, y=value)))
 
 
-# Review, QA, and Repeat --------------------------------------------------
+cdec_daily_SJS_QA <- rbind(cdec_daily_SJS_QA, cdec_daily_SJS_4001_5000)
 
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_SJS[5001:6164,], aes(x=datetime, y=value)))
+
+cdec_daily_SJS_5001_6164 <- cdec_daily_SJS[5001:6164,] %>% 
+  filter(value < 93.3, value > 32, datetime != "2019-04-30", datetime != "2019-04-25")
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_SJS_5001_6164, aes(x=datetime, y=value)))
+
+cdec_daily_SJS_QA <- rbind(cdec_daily_SJS_QA, cdec_daily_SJS_5001_6164)
 
 
 
@@ -83,7 +103,7 @@ gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
 gage_QA_progress[95,1]
 
 #note reviewer initials, whether review is complete, and any final notes
-gage_QA_progress[95,4:6] <- c("ADW", "N", "QA in progress")
+gage_QA_progress[95,4:6] <- c("ADW", "Y", "QA complete")
 
 #save updated dataframe to the .csv
 write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
