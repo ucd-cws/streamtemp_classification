95e6740ba495a303e0e0038b15a9ac9f1e495040
Ann Willis <awillis@ucdavis.edu>
2020-01-10 10:54:32 -0800

    Saved most up-to-date version of code.

--- code/QA_code/USGS_daily_11446500_QA.R
+++ code/QA_code/USGS_daily_11446500_QA.R
@@ -31,22 +31,22 @@ ggplotly(
 # Final review ------------------------------------------------------------
 
 #plot QA'd dataset to confirm all points look good
-ggplotly(
-  ggplot() +geom_point(data = usgs_daily_11446500_QA, aes(x=Date, y=X_00010_00003)))
-
-# Data is not continuous; however, we still have enough data for the analysis, so we will keep the site.
-
-# #save QA'd dataset as a .rds file
-write_rds(usgs_daily_11446500_QA, path = "data/QA_data/usgs_daily_11446500_QA.rds")
+# ggplotly(
+#   ggplot() +geom_point(data = usgs_daily_11446500_QA, aes(x=Date, y=X_00010_00003)))
+# 
+# # Data is not continuous; however, we still have enough data for the analysis, so we will keep the site.
+# 
+# # #save QA'd dataset as a .rds file
+# write_rds(usgs_daily_11446500_QA, path = "data/QA_data/usgs_daily_11446500_QA.rds")
 
 #update the gage_QA_progress
 gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
 
 #confirm correct row to update by the site_id
-gage_QA_progress[194,1]
+gage_QA_progress[191,1]
 
 #note reviewer initials, whether review is complete, and any final notes
-gage_QA_progress[194,4:6] <- c("ADW", "Y", "QA complete")
+gage_QA_progress[191,4:6] <- c("ADW", "Y", "Data gap too large; gage dropped from study")
 
 #save updated dataframe to the .csv
 write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
