caac10acefa654262ef27860125093d3a13eedb3
Ann Willis <awillis@ucdavis.edu>
2020-01-15 16:05:52 -0800

    Saved final version of BNF code.

--- code/QA_code/CDEC_daily_BNF_QA.R
+++ code/QA_code/CDEC_daily_BNF_QA.R
@@ -1,7 +1,7 @@
 
 # Code description --------------------------------------------------------
 
-# Code to review temperature data from gage CDEC_BIC.
+# Code to review temperature data from gage CDEC_BNF.
 
 library(tidyverse)
 library(lubridate)
@@ -13,86 +13,64 @@ library(plotly)
 file_list <- list.files("data/data_review/")
 
 # read in next file:
-file_list[[7]]
+file_list[[8]]
 
-cdec_daily_BIC <- read_rds(path = paste0("data/data_review/",file_list[[7]]))
+cdec_daily_BNF <- read_rds(path = paste0("data/data_review/",file_list[[8]]))
 
 # Plot --------------------------------------------------------------------
 
 #plot all data to look for obvious bad data points and gaps
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[,], aes(x=date, y=value_mean_C)))
+  ggplot() + geom_point(data=cdec_daily_BNF[,], aes(x=date, y=value_mean_C)))
 
 # now make an interactive plot of first 1000 values
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[1:1000,], aes(x=date, y=value_mean_C)))
+  ggplot() + geom_point(data=cdec_daily_BNF[1:1000,], aes(x=date, y=value_mean_C)))
 
 # Review, QA, and Repeat --------------------------------------------------
-cdec_daily_BIC_1_1000 <- cdec_daily_BIC[1:1000,] %>% 
-  filter(value_mean_C > 0, value_mean_C < 30, date != "2001-06-06", date != "2001-06-07")
+cdec_daily_BNF_QA <- cdec_daily_BNF[1:1000,]
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC_1_1000, aes(x=date, y=value_mean_C)))
+  ggplot() + geom_point(data=cdec_daily_BNF[1001:2000,], aes(x=date, y=value_mean_C)))
 
-cdec_daily_BIC_QA <- cdec_daily_BIC_1_1000
-
-ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[1001:2000,], aes(x=date, y=value_mean_C)))
-
-cdec_daily_BIC_1001_2000 <- cdec_daily_BIC[1001:2000,] %>% 
-  filter(value_mean_C > 3.00)
+cdec_daily_BNF_QA <- rbind(cdec_daily_BNF_QA,cdec_daily_BNF[1001:2000,])
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC_1001_2000, aes(x=date, y=value_mean_C)))
+  ggplot() + geom_point(data=cdec_daily_BNF[2001:3000,], aes(x=date, y=value_mean_C)))
 
-cdec_daily_BIC_QA <- rbind(cdec_daily_BIC_QA, cdec_daily_BIC_1001_2000)
-
-ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[2001:3000,], aes(x=date, y=value_mean_C)))
-
-cdec_daily_BIC_2001_3000 <- cdec_daily_BIC[2001:3000,] %>% 
-  filter(value_mean_C < 29.0)
+cdec_daily_BNF_QA <- rbind(cdec_daily_BNF_QA,cdec_daily_BNF[2001:3000,])
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC_2001_3000, aes(x=date, y=value_mean_C)))
+  ggplot() + geom_point(data=cdec_daily_BNF[3001:4000,], aes(x=date, y=value_mean_C)))
 
-cdec_daily_BIC_QA <- rbind(cdec_daily_BIC_QA, cdec_daily_BIC_2001_3000)
-
-ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[3001:4000,], aes(x=date, y=value_mean_C)))
-
-cdec_daily_BIC_QA <- rbind(cdec_daily_BIC_QA, cdec_daily_BIC[3001:4000,])
+cdec_daily_BNF_QA <- rbind(cdec_daily_BNF_QA, cdec_daily_BNF[3001:4000,])
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[4001:5000,], aes(x=date, y=value_mean_C)))
+  ggplot() + geom_point(data=cdec_daily_BNF[4001:5000,], aes(x=date, y=value_mean_C)))
 
-cdec_daily_BIC_4001_5000 <- cdec_daily_BIC[4001:5000,] %>% 
-  filter(value_mean_C > 0)
+cdec_daily_BNF_4001_5000 <- cdec_daily_BNF[4001:5000,]
 
-ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC_4001_5000, aes(x=date, y=value_mean_C)))
+cdec_daily_BNF_4001_5000 <- cdec_daily_BNF_4001_5000[-c(913:976),]
 
-cdec_daily_BIC_QA <- rbind(cdec_daily_BIC_QA,cdec_daily_BIC_4001_5000)
+cdec_daily_BNF_4001_5000 <- cdec_daily_BNF_4001_5000[-c(535:547),]
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_BIC[5001:5838,], aes(x=date, y=value_mean_C)))
-
-cdec_daily_BIC_QA <- rbind(cdec_daily_BIC_QA, cdec_daily_BIC[5001:5838,])
+  ggplot() + geom_point(data=cdec_daily_BNF_4001_5000, aes(x=date, y=value_mean_C)))
 
 # Final review ------------------------------------------------------------
 
 #plot QA'd dataset to confirm all points look good
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_BIC_QA, aes(x=date, y=value_mean_C)))
+  ggplot() +geom_point(data = cdec_daily_BNF_QA, aes(x=date, y=value_mean_C)))
 
 #save QA'd dataset as a .rds file
-write_rds(cdec_daily_BIC_QA, path = "data/QA_data/cdec_daily_BIC_QA.rds")
+write_rds(cdec_daily_BNF_QA, path = "data/QA_data/cdec_daily_BNF_QA.rds")
 
 #update the gage_QA_progress
 gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
 
 #note reviewer initials, whether review is complete, and any final notes
-gage_QA_progress[gage_QA_progress$site_id=="BIC",4:6] <- c("ADW", "Y", "QA complete")
+gage_QA_progress[gage_QA_progress$site_id=="BNF",4:6] <- c("ADW", "Y", "QA complete")
 
 #save updated dataframe to the .csv
 write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
