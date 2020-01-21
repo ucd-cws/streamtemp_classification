6c24d78076f97b80a27173449be73d7f65003611
Ann Willis <awillis@ucdavis.edu>
2020-01-21 14:26:12 -0800

    completed QA for cdec gage FHH. Saved code and data files. Updated gage_QA_progress table.

---
+++ code/QA_code/CDEC_daily_FHH_QA.R
@@ -0,0 +1,59 @@
+
+# Code description --------------------------------------------------------
+
+# Code to review temperature data from gage CDEC_FHH.
+
+library(tidyverse)
+library(lubridate)
+library(weathermetrics)
+library(plotly)
+
+# Get Data ----------------------------------------------------------------
+
+file_list <- list.files("data/data_review/")
+
+# read in next file:
+file_list[[19]]
+
+cdec_daily_FHH <- read_rds(path = paste0("data/data_review/",file_list[[19]]))
+
+# Plot --------------------------------------------------------------------
+
+#plot all data to look for obvious bad data points and gaps
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_FHH[,], aes(x=date, y=value_mean_C)))
+
+# now make an interactive plot of first 1000 values
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_FHH[1:1000,], aes(x=date, y=value_mean_C)))
+
+# Review, QA, and Repeat --------------------------------------------------
+cdec_daily_FHH_QA <- cdec_daily_FHH[1:1000,]
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_FHH[1001:2000,], aes(x=date, y=value_mean_C)))
+
+cdec_daily_FHH_QA <- rbind(cdec_daily_FHH_QA, cdec_daily_FHH[1001:2000,])
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_FHH[2001:3141,], aes(x=date, y=value_mean_C)))
+
+cdec_daily_FHH_QA <- rbind(cdec_daily_FHH_QA, cdec_daily_FHH[2001:3141,])
+
+# Final review ------------------------------------------------------------
+
+#plot QA'd dataset to confirm all points look good
+ggplotly(
+  ggplot() +geom_point(data = cdec_daily_FHH_QA, aes(x=date, y=value_mean_C)))
+
+#save QA'd dataset as a .rds file
+write_rds(cdec_daily_FHH_QA, path = "data/QA_data/cdec_daily_FHH_QA.rds")
+
+#update the gage_QA_progress
+gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
+
+#note reviewer initials, whether review is complete, and any final notes
+gage_QA_progress[gage_QA_progress$site_id=="FHH",4:6] <- c("ADW", "Y", "QA complete")
+
+#save updated dataframe to the .csv
+write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
