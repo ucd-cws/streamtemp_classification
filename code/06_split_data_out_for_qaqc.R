ca79c84346360b44b5f816f67b82394858b2e2ab
Ryan Peek <rapeek@ucdavis.edu>
2020-01-22 17:13:37 -0800

    update code to get final updated gage list for QA review

--- code/06_split_data_out_for_qaqc.R
+++ code/06_split_data_out_for_qaqc.R
@@ -2,6 +2,7 @@
 
 library(sf)
 library(tidyverse)
+library(tidylog)
 library(purrr)
 library(lubridate)
 
@@ -45,3 +46,34 @@ usgs_daily %>%
 
 # check number of files matches number of total gages?
 length(list.files("data/data_review", pattern = ".rds")) ## should match cdec_stations == 63 + usgs_metadata_filt8 == 22 (n=95)
+
+
+
+# Get Final List of Gages -------------------------------------------------
+
+(file_list <-  tools::file_path_sans_ext(list.files("data/data_review/", pattern = ".rds")) %>% tibble::enframe(name = NULL, value = "filename")) # so 96 gages that we have data to review
+
+# split out file names 
+file_list <- file_list %>% 
+  separate(col = filename, into = c("type", "interval", "site_id"), sep = "_")
+
+# match with csv
+# site_id,site_name,operator,reviewer,completed(Y/N),notes
+
+load("data/all_gages.rda")
+all_gages %>% st_drop_geometry() -> all_gages
+
+# read in existing CSV
+gage_QA <- read_csv("data/data_review/gage_QA_progress.csv") %>% 
+  rename(completed_Y_N = `completed(Y/N)`) # fix col name
+
+# join with existing (filter > 8 yrs) file list:
+gage_QA_revised <- left_join(file_list[,c(3)], gage_QA, by="site_id")
+
+# join lat lon back in to make maps easily:
+gage_QA_revised <- left_join(gage_QA_revised, all_gages[,c(1,3,4)], by="site_id") %>% 
+  select(site_id, site_name, lon, lat, operator:notes)
+
+# write out as csv
+write_csv(gage_QA_revised, "data/data_review/gage_QA_progress_revised.csv")
+
