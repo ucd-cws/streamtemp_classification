b8fbaa01e9f3b5a5ae55fe40e12a15e56f14dbbd
Ann Willis <awillis@ucdavis.edu>
2020-01-29 14:54:05 -0800

    Completed QA for usgs gage 10261500. Insufficient data for full analysis. Saved code. No data file exported. Updated gage_QA_progress table.

---
+++ code/QA_code/USGS_daily_10261500_QA.R
@@ -0,0 +1,52 @@
+
+# Code description --------------------------------------------------------
+
+# Code to review temperature data from gage USGS_10261500.
+
+library(tidyverse)
+library(lubridate)
+library(weathermetrics)
+library(plotly)
+
+# Get Data ----------------------------------------------------------------
+
+file_list <- list.files("data/data_review/", pattern = ".rds")
+
+# read in next file:
+file_list[[65]]
+
+usgs_daily_10261500 <- read_rds(path = paste0("data/data_review/",file_list[[65]]))
+
+# Plot --------------------------------------------------------------------
+
+#plot all data to look for obvious bad data points and gaps
+ggplotly(
+  ggplot() + geom_point(data=usgs_daily_10261500[,], aes(x=date, y=value_mean_C)))
+
+# Insufficient data for full analysis. Site dropped from study.
+
+# now make an interactive plot of first 1000 values
+ggplotly(
+  ggplot() + geom_point(data=usgs_daily_10261500[1:1000,], aes(x=date, y=value_mean_C)))
+
+# Review, QA, and Repeat --------------------------------------------------
+
+
+
+# Final review ------------------------------------------------------------
+
+#plot QA'd dataset to confirm all points look good
+ggplotly(
+  ggplot() +geom_point(data = usgs_daily_10261500_QA, aes(x=date, y=value_mean_C)))
+
+#save QA'd dataset as a .rds file
+write_rds(usgs_daily_10261500_QA, path = "data/QA_data/usgs_daily_10261500_QA.rds")
+
+#update the gage_QA_progress
+gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
+
+#note reviewer initials, whether review is complete, and any final notes
+gage_QA_progress[gage_QA_progress$site_id=="10261500",6:8] <- c("ADW", "Y", "Insufficient data for full analysis. Site dropped from study.")
+
+#save updated dataframe to the .csv
+write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
