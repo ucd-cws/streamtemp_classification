b16b3aee97c0f1bb637087e421ac93f21c1989f8
Ann Willis <awillis@ucdavis.edu>
2020-01-29 11:46:46 -0800

    Completed QA for cdec station TGC. Saved code and data file. Updated gage_QA_progress spreadsheet.

---
+++ code/QA_code/CDEC_daily_TGC_QA.R
@@ -0,0 +1,71 @@
+
+# Code description --------------------------------------------------------
+
+# Code to review temperature data from gage CDEC_TGC.
+
+library(tidyverse)
+library(lubridate)
+library(weathermetrics)
+library(plotly)
+
+# Get Data ----------------------------------------------------------------
+
+file_list <- list.files("data/data_review/", pattern = ".rds")
+
+# read in next file:
+file_list[[58]]
+
+cdec_daily_TGC <- read_rds(path = paste0("data/data_review/",file_list[[58]]))
+
+# Plot --------------------------------------------------------------------
+
+#plot all data to look for obvious bad data points and gaps
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_TGC[,], aes(x=date, y=value_mean_C)))
+
+# now make an interactive plot of first 1000 values
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_TGC[1:1000,], aes(x=date, y=value_mean_C)))
+
+# Review, QA, and Repeat --------------------------------------------------
+cdec_daily_TGC_QA <- cdec_daily_TGC[1:1000,]
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_TGC[1001:2000,], aes(x=date, y=value_mean_C)))
+
+cdec_daily_TGC_1001_2000 <- cdec_daily_TGC[1001:1976,]
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_TGC_1001_2000, aes(x=date, y=value_mean_C)))
+
+cdec_daily_TGC_QA <- rbind(cdec_daily_TGC_QA, cdec_daily_TGC_1001_2000)
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_TGC[2001:3000,], aes(x=date, y=value_mean_C)))
+
+cdec_daily_TGC_2001_3000 <- cdec_daily_TGC[2903:3000,]
+
+cdec_daily_TGC_QA <- rbind(cdec_daily_TGC_QA, cdec_daily_TGC_2001_3000)
+
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_TGC[3001:4264,], aes(x=date, y=value_mean_C)))
+
+cdec_daily_TGC_QA <- rbind(cdec_daily_TGC_QA, cdec_daily_TGC[3001:4264,])
+
+# Final review ------------------------------------------------------------
+
+#plot QA'd dataset to confirm all points look good
+ggplotly(
+  ggplot() +geom_point(data = cdec_daily_TGC_QA, aes(x=date, y=value_mean_C)))
+
+#save QA'd dataset as a .rds file
+write_rds(cdec_daily_TGC_QA, path = "data/QA_data/cdec_daily_TGC_QA.rds")
+
+#update the gage_QA_progress
+gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
+
+#note reviewer initials, whether review is complete, and any final notes
+gage_QA_progress[gage_QA_progress$site_id=="TGC",6:8] <- c("QA complete")
+
+#save updated dataframe to the .csv
+write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
