b9fde533b6d372cdcf7c4f31acab67de0e7e2e05
Ann Willis <awillis@ucdavis.edu>
2020-02-06 15:16:14 -0800

    Re-did QA for cdec gage SWA. Saved code and data files.

--- code/QA_code/CDEC_daily_SWA_QA.R
+++ code/QA_code/CDEC_daily_SWA_QA.R
@@ -13,57 +13,57 @@ library(plotly)
 file_list <- list.files("data/data_review/")
 
 # read in next file:
-file_list[[7]]
+file_list[[56]]
 
-cdec_daily_SWA <- read_rds(path = paste0("data/data_review/",file_list[[7]]))
+cdec_daily_SWA <- read_rds(path = paste0("data/data_review/",file_list[[56]]))
 
 
 # Plot --------------------------------------------------------------------
 
 # now make an interactive plot of first 1000 values
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA[1:1000,], aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA[1:1000,], aes(x=date, y=value_mean_C)))
 
 # Review, QA, and Repeat --------------------------------------------------
 
 cdec_daily_SWA_1_1000 <- cdec_daily_SWA[1:1000,] %>% 
-  filter(value > 33.0)
+  filter(value_mean_C > fahrenheit.to.celsius(33.0))
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA_1_1000, aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA_1_1000, aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_QA <- cdec_daily_SWA_1_1000
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA[1001:2000,], aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA[1001:2000,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_1001_2000 <- cdec_daily_SWA[1001:2000,] %>% 
-  filter(value > 38.2)
+  filter(value_mean_C > fahrenheit.to.celsius(38.2))
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA_1001_2000, aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA_1001_2000, aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_QA <- rbind(cdec_daily_SWA_QA,cdec_daily_SWA_1001_2000)
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA[2001:3000,], aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA[2001:3000,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_2001_3000 <- cdec_daily_SWA[2001:3000,] %>% 
-  filter(value > 29.5, value < 100, datetime != "2016-09-08")
+  filter(value_mean_C > fahrenheit.to.celsius(29.5), date != "2016-09-08")
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA_2001_3000, aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA_2001_3000, aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_QA <- rbind(cdec_daily_SWA_QA, cdec_daily_SWA_2001_3000)
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA[3001:3168,], aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA[3001:3168,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_3001_3168 <- cdec_daily_SWA[3001:3168,] %>% 
-  filter(value > 29.3)
+  filter(value_mean_C > fahrenheit.to.celsius(29.3))
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_SWA_3001_3168, aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_SWA_3001_3168, aes(x=date, y=value_mean_C)))
 
 cdec_daily_SWA_QA <- rbind(cdec_daily_SWA_QA, cdec_daily_SWA_3001_3168)
 
@@ -71,7 +71,7 @@ cdec_daily_SWA_QA <- rbind(cdec_daily_SWA_QA, cdec_daily_SWA_3001_3168)
 
 #plot QA'd dataset to confirm all points look good
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_SWA_QA, aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_SWA_QA, aes(x=date, y=value_mean_C)))
 
 #save QA'd dataset as a .rds file
 write_rds(cdec_daily_SWA_QA, path = "data/QA_data/cdec_daily_SWA_QA.rds")
