354826c6adc14204c088d893579e274a0638418a
Ann Willis <awillis@ucdavis.edu>
2020-02-06 13:23:52 -0800

    Worked on the compile_QA_into_model_data code; found issues with cdec hourly stations that had been processed prior to converting all datasets to daily mean. Checking in current changes and then reviewing QA data from hourly files next on Ann_QA_review branch.

--- code/QA_code/CDEC_daily_CRS_QA.R
+++ code/QA_code/CDEC_daily_CRS_QA.R
@@ -12,81 +12,84 @@ library(lubridate)
 file_list <- list.files("data/data_review/")
 
 # read in next file:
-file_list[[1]]
+file_list[[14]]
 
-cdec_daily_CRS <- read_rds(path = paste0("data/data_review/",file_list[[1]]))
+cdec_daily_CRS <- read_rds(path = paste0("data/data_review/",file_list[[14]]))
 
 # Plot --------------------------------------------------------------------
 
 library(plotly)
 
-# now make an interactive plot of first 1000 values
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_CRS[1:1000,], aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_CRS[,], aes(x=date, y=value_mean_C)))
+
+# now make an interactive plot of first 1000 value_mean_C_mean_Cs
+ggplotly(
+  ggplot() + geom_point(data=cdec_daily_CRS[1:1000,], aes(x=date, y=value_mean_C)))
 
 
 # Review, QA, and Repeat --------------------------------------------------
 
 cdec_daily_CRS_1_1000 <- cdec_daily_CRS[1:1000,] %>%
-  filter(datetime != "2000-10-31")
+  filter(date != "2000-10-31")
 
 ggplotly(
-  ggplot() + geom_point(data=cdec_daily_CRS_1_1000[1:1000,], aes(x=datetime, y=value)))
+  ggplot() + geom_point(data=cdec_daily_CRS_1_1000[1:1000,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_QA <- cdec_daily_CRS_1_1000
 
 # make an interactive plot of points 1001-2000
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS[1001:2000,], aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS[1001:2000,], aes(x=date, y=value_mean_C)))
 
 #points look good, bind to QA'd dataframe
 cdec_daily_CRS_QA <- rbind(cdec_daily_CRS_QA, cdec_daily_CRS[1001:2000,])
 
 #make an interactive plot of points 2001-3000
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS[2001:3000,], aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS[2001:3000,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_2001_3000 <- cdec_daily_CRS[2001:3000,] %>% 
-  filter(value > 37)
+  filter(value_mean_C > 2.67)
 
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS_2001_3000, aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS_2001_3000, aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_QA <- rbind(cdec_daily_CRS_QA, cdec_daily_CRS_2001_3000)
 
 #make an interactive plot of points 3001-4000
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS[3001:4000,], aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS[3001:4000,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_QA <- rbind(cdec_daily_CRS_QA, cdec_daily_CRS[3001:4000,])
 
 #make an interactive plot of points 4001-5000
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS[4001:5000,], aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS[4001:5000,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_4001_5000 <- cdec_daily_CRS[4001:5000,] %>% 
-  filter(value > 43.9)
+  filter(value_mean_C > 6.61)
 
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS_4001_5000, aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS_4001_5000, aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_QA <- rbind(cdec_daily_CRS_QA, cdec_daily_CRS_4001_5000)
 
 #make an interactive plot of 5001-6486
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS[5001:6486,], aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS[5001:6486,], aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_5001_6486 <- cdec_daily_CRS[5001:6486,] %>% 
-  filter(value > 34.8)
+  filter(value_mean_C > 1.56)
 
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS_5001_6486, aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS_5001_6486, aes(x=date, y=value_mean_C)))
 
 cdec_daily_CRS_QA <- rbind(cdec_daily_CRS_QA, cdec_daily_CRS_5001_6486)
 
 #plot QA'd dataset to confirm all points look good
 ggplotly(
-  ggplot() +geom_point(data = cdec_daily_CRS_QA, aes(x=datetime, y=value)))
+  ggplot() +geom_point(data = cdec_daily_CRS_QA, aes(x=date, y=value_mean_C)))
 
 #save QA'd dataset as a .rds file
 write_rds(cdec_daily_CRS_QA, path = "data/QA_data/cdec_daily_CRS_QA.rds")
