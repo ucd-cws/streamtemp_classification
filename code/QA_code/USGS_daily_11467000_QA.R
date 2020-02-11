9501becffdfde01b94549d6696b660dbd626c182
Ann Willis <awillis@ucdavis.edu>
2020-02-11 11:07:19 -0800

    Updated format of QA data file for usgs gage 11467000. Saved code and data files.

--- code/QA_code/USGS_daily_11467000_QA.R
+++ code/QA_code/USGS_daily_11467000_QA.R
@@ -13,41 +13,41 @@ library(plotly)
 file_list <- list.files("data/data_review/")
 
 # read in next file:
-file_list[[89]]
+file_list[[94]]
 
-usgs_daily_11467000 <- read_rds(path = paste0("data/data_review/",file_list[[89]]))
+usgs_daily_11467000 <- read_rds(path = paste0("data/data_review/",file_list[[94]]))
 
 
 # Plot --------------------------------------------------------------------
 
 # now make an interactive plot of first 1000 values
 ggplotly(
-  ggplot() + geom_point(data=usgs_daily_11467000[1:1000,], aes(x=Date, y=X_00010_00003)))
+  ggplot() + geom_point(data=usgs_daily_11467000[1:1000,], aes(x=date, y=value_mean_C)))
 
 # Review, QA, and Repeat --------------------------------------------------
 
 usgs_daily_11467000_QA <- usgs_daily_11467000[1:1000,]
 
 ggplotly(
-  ggplot() + geom_point(data=usgs_daily_11467000[1001:2000,], aes(x=Date, y=X_00010_00003)))
+  ggplot() + geom_point(data=usgs_daily_11467000[1001:2000,], aes(x=date, y=value_mean_C)))
 
 usgs_daily_11467000_QA <- usgs_daily_11467000[1:2000,]
 
 ggplotly(
-  ggplot() + geom_point(data=usgs_daily_11467000[2001:3000,], aes(x=Date, y=X_00010_00003)))
+  ggplot() + geom_point(data=usgs_daily_11467000[2001:3000,], aes(x=date, y=value_mean_C)))
 
 usgs_daily_11467000_QA <- usgs_daily_11467000[1:3000,]
 
 ggplotly(
-  ggplot() + geom_point(data=usgs_daily_11467000[3001:4150,], aes(x=Date, y=X_00010_00003)))
+  ggplot() + geom_point(data=usgs_daily_11467000[3001:3965,], aes(x=date, y=value_mean_C)))
 
-usgs_daily_11467000_QA <- usgs_daily_11467000[1:4150,]
+usgs_daily_11467000_QA <- usgs_daily_11467000[1:3965,]
 
 # Final review ------------------------------------------------------------
 
 #plot QA'd dataset to confirm all points look good
 ggplotly(
-  ggplot() +geom_point(data = usgs_daily_11467000_QA, aes(x=Date, y=X_00010_00003)))
+  ggplot() +geom_point(data = usgs_daily_11467000_QA, aes(x=date, y=value_mean_C)))
 
 # Data is not continuous; gap exists from 09-2010 until 10-2016; however, we still have enough data for the analysis, so we will keep the site.
 
@@ -58,7 +58,7 @@ write_rds(usgs_daily_11467000_QA, path = "data/QA_data/usgs_daily_11467000_QA.rd
 gage_QA_progress <- read_csv("data/data_review/gage_QA_progress.csv")
 
 #note reviewer initials, whether review is complete, and any final notes
-gage_QA_progress[gage_QA_progress$site_id=="11467000",4:6] <- c("ADW", "Y", "QA complete")
+gage_QA_progress[gage_QA_progress$site_id=="11467000",6:8] <- c("ADW", "Y", "QA complete")
 
 #save updated dataframe to the .csv
 write_csv(gage_QA_progress, path = "data/data_review/gage_QA_progress.csv")
