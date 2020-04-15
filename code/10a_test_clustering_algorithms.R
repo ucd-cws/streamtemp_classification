42261240b76555eacfd8d6edd4ea54364e2fd49c
Ryan Peek <rapeek@ucdavis.edu>
2020-04-14 21:31:04 -0700

    update code for k groups

--- code/10a_test_clustering_algorithms.R
+++ code/10a_test_clustering_algorithms.R
@@ -1,97 +1,19 @@
-## Calculate Metrics from Modeled Data for Classification
-# following Maheu et al. (2015) 
-# - annual mean
-# - annual amplitude (annual max - annual mean)
-# - day of annual maximum
+## Clustering by thermal regimes
 
 # Libraries ---------------------------------------------------------------
 
 library(tidyverse)
 library(tidylog)
-#library(slider) # for sliding window calculations: https://davisvaughan.github.io/slider/
-
-# Get Data ----------------------------------------------------------------
-
-load("output/models/thermal_regime_models_daily.rda")
-
-# do some basic cleaning to drop attributes
-model_out <- model_out %>% as.data.frame()
-
-
-# Calculate Annual Mean & Amplitude By Gage ------------------------------
-
-# annual amplitude is the annual max - annual mean
-ann_metrics <- model_out %>% 
-  group_by(station_id) %>% 
-  summarize(ann_mean = mean(model_avg_daily_temp_C),
-            ann_max = max(model_avg_daily_temp_C),
-            ann_amp = ann_max - ann_mean)
-
-# visualize:
-(gg1 <- ggplot() + 
-  geom_point(data=ann_metrics, aes(x=reorder(station_id, ann_amp), y=ann_amp), pch=21, fill="purple2", size=2.5, alpha=0.8) +
-    coord_flip() +
-    theme_classic(base_family = "Roboto Condensed") + # may need to change this to "Helvetica" or "Arial"
-    theme(legend.position = c(.1, 0.8),
-          axis.text.y = element_text(size=7),
-          panel.grid.major.y = element_line(color="gray80", size=0.1))+
-    labs(x="Station ID", 
-         y="Annual Amplitude (ann. max - ann. mean) (C)"))
-  
-(gg2 <- ggplot() +
-    geom_point(data=ann_metrics, aes(x=reorder(station_id, ann_mean), y=ann_mean), color="darkblue",size=2.5, alpha=0.8) +
-      coord_flip()+
-      theme_classic(base_family = "Roboto Condensed") +
-      theme(legend.position = c(.1, 0.8),
-            axis.text.y = element_text(size=7),
-            panel.grid.major.y = element_line(color="gray80", size=0.1))+
-      labs(x="", 
-           y="Annual Mean (C)"))
-
-library(cowplot)
-(pg1 <- plot_grid(gg1, gg2, labels = c("A","B"), label_fontfamily = "Roboto Condensed"))
-#save_plot(pg1, filename = "output/figures/ann_mean_ann_amp_cowplot.png",base_height = 6, dpi=300)
-
-# Calculate Day of Ann Max ------------------------------------------------
-
-ann_max_day <- model_out %>% 
-  group_by(station_id) %>% 
-  slice(which.max(model_avg_daily_temp_C)) %>% 
-  ungroup()
-
-#library(plotly)
-#ggplotly(
-  ggplot() +
-  geom_point(data=ann_max_day, aes(x=reorder(station_id, DOWY), y=DOWY, 
-                                   color=model_avg_daily_temp_C)) +
-    coord_flip()+
-    scale_color_viridis_c("Modeled Avg \n Daily Temp (C)") +
-    theme_classic() +
-    theme(legend.position = c(.1, 0.8),
-          panel.grid.major.y = element_line(color="gray80", size=0.1))+
-    labs(x="Station ID", 
-         y="Day of Annual Max (Day of Water Year)")
-#)
-#ggsave("output/figures/day_of_ann_max_dowy.png", dpi=300, width = 9, height = 7, units = "in")
-
-# Join Together -----------------------------------------------------------
-
-ann_metrics <- left_join(ann_metrics, ann_max_day)
-
-# should be 78 rows for 78 sites!
-nrow(ann_metrics)
-
-# save
-#save(ann_metrics, file = "output/models/annual_cluster_metrics_all_gages.rda")
-
-# done! 
-
-# CLUSTERING: Scale & Create Dist Matrix ------------------------------------------------
-
 library(cluster)    # clustering algorithms
 library(factoextra) # clustering visualization/stats
 
-# first double check for NAs
+# Load Data ---------------------------------------------------------------
+
+load("output/models/annual_cluster_metrics_all_gages.rda")
+
+# CLUSTERING: Scale & Create Dist Matrix ------------------------------------------------
+
+# first double check for NAs, total rows=77
 summary(ann_metrics)
 
 # then scale data (mean of zero, sd =1)
@@ -103,137 +25,86 @@ ann_metrics_s <- ann_metrics %>%
 # create Euclidean dissimilarity/distance matrix
 d1 <- dist(ann_metrics_s, method = "euclidean")
 
-# calc clusters and get means
-tst1 <- factoextra::hkmeans(ann_metrics_s, 5)
-hkmeans_tree(tst1, viridis::viridis(5))
-fviz_cluster(tst1, show.clust.cent = T, ggtheme = theme_bw())
+# 01. {factoextra} Check data uniformity ----------------------------------
 
-tst1$centers
+# use Hopkins Stat to assess data uniformity:
+## if data highly uniform, clustering not an ideal approach
+## small values of $hopkins_stat indicate uniform data (normalized deviation from uniform data)
+## higher values mean the data is not uniform and suitable for clustering...some use a threshold
+## roughtly, if H > 0.5, then "suitable for clustering" but this seems a bit vague/contentious.
 
-# HCLUST: {hclust} ------------------------------------------------------------------
-
-## Agglomerative Nesting Clustering
-
-# Hierarchical clustering using Complete Linkage
-hc1 <- hclust(d1, method = "ward.D2")
-
-# PLOT Dendro
-plot(hc1, cex = 0.6, hang = -1)
-
-# see how many clusters with k = 5 (totally arbitrary)
-rect.hclust(hc1, k = 5, border = viridis::viridis(5))
-
-# try with k=3
-plot(hc1, cex = 0.6, hang = -1, main="Dendrogram {hclust}: Wards")
-rect.hclust(hc1, k = 3, border = viridis::viridis(3))
-
-# HCLUST: {agnes} -------------------------------------------------------
-
-# Agglomerative Nesting Clustering
-
-# use agnes to get agglomerative coefficient
-# closer to 1 is stronger clusterings
-hc2 <- agnes(d1, method = "ward")
-hc2$ac
-
-# try diff methods to assess
-m <- c( "average", "single", "complete", "ward")
-names(m) <- c( "average", "single", "complete", "ward")
-
-# function to compute coefficient
-ac <- function(x) {
-  agnes(d1, method = x)$ac
-}
-
-# so wards is still best option here...
-map_dbl(m, ac)
-
-# plot and add clusters
-pltree(hc2, cex = 0.6, hang = -1, main = "Dendrogram {agnes}: Wards") 
-rect.hclust(hc2, k = 5, border = viridis::viridis(5))
-
-# try with k=3
-pltree(hc2, cex = 0.6, hang = -1, main = "Dendrogram {agnes}: Wards") 
-rect.hclust(hc2, k = 3, border = viridis::viridis(3))
-
-# the groups out
-hc2_grps_k5 <- cutree(hc2, k=5) # try k=5
-table(hc2_grps_k5)
-hc2_grps_k3 <- cutree(hc2, k=3) # try k=3
-
-# bind together and save
-agnes_k_groups <- tibble(site_id = ann_metrics$station_id, k_3=hc2_grps_k3, k_5=hc2_grps_k5)
-save(agnes_k_groups, file="output/models/agnes_k_groups_final.rda")
-
-# plot
-ggclust2_k5 <- fviz_cluster(list(data=d1, cluster=hc2_grps_k5))
-ggclust2_k5 + theme_classic() +
-  labs(title = "Clusters for CA Thermal Regimes (k=5)")
-ggsave("output/figures/pc_agnes_k5.png", width = 8, height = 6, units="in", dpi=300)
-
-# plot k=3
-ggclust2_k3 <- fviz_cluster(list(data=d1, cluster=hc2_grps_k3))
-ggclust2_k3 + theme_classic() +
-  labs(title = "Clusters for CA Thermal Regimes (k=3)")
-#ggsave("output/figures/pc_agnes_k3.png", width = 8, height = 6, units="in", dpi=300)
-
-# HCLUST: {NbClust} --------------------------------------------------------
-
-# use Hopkins Stat to assess data uniformity
-# if data highly uniform, clustering not an ideal approach
-# small values of $hopkins_stat indicate uniform data (normalized deviation from uniform data)
-# higher values mean the data is not uniform and suitable for clustering...some use a threshold
-# of H > 0.5 means "suitable for clustering" but this seems a bit vague/contentious.
 factoextra::get_clust_tendency(ann_metrics_s, 
-                               n = round(.9*nrow(ann_metrics_s), 0), # take 90% of data
+                               # take 90% of data
+                               n = round(.9*nrow(ann_metrics_s), 0), 
                                seed = 123)
-# so value of our data is: 0.89 (so very non-uniform!)
+# so value of our data is: 0.89 (so very non-uniform!, and thus good for clustering
+
+# 02. {NbClust} Check for Ideal K -----------------------------------------
 
 # now check for ideal K using 'NbClust()'
 library(NbClust)
-NbClust(ann_metrics_s, distance = "euclidean", method = "ward.D2")
-# so based on this, a k=3 is best, but 5 methods provided k=5
+
+(clust_stats <- NbClust(ann_metrics_s, distance = "euclidean", method = "ward.D2"))
+
+# so based on this, a consensus of k=3 is best, but can justify k=5 based on 2 methods. (Silhouette and PtBiserial?)
 
 dev.off() # reset graphics
 
-# HCLUST: {diana} ---------------------------------------------------------
+# 03. {fpc} Cluster Stability --------------------------------------------
 
-## Trying with the DIANA model (Divisive Analysis Clustering)
-hc3 <- diana(d1, metric = "euclidean", stand = FALSE)
-# plot
-pltree(hc3, cex=0.6, hang=-1, "Dendrogram {diana}: Euclidean")
-rect.hclust(hc3, k=5, border = viridis::viridis(5))
+library(fpc)
 
-library(ggdendro)
-ggdendrogram(hc3, rotate=TRUE) + theme_classic(base_family = "Roboto Condensed")
+# test stability of k=5
 
-# look at cluster divisive coefficient: 
-hc3$dc # very high, means good clusters
+# try with hclust k=5
+cboot5 <- clusterboot(data = d1, 
+                      clustermethod = hclustCBI,
+                      method="ward.D2",
+                      seed = 333,
+                      k=5,
+                      B = 100 # number of resamples/bootstraps
+)
+print.clboot(cboot5) # look at all outputs
 
-# cut the clusters
-hc3_grps <- cutree(hc3, k=5)
-table(hc3_grps) # gives the groups
+# try with hclust k=3
+cboot3 <- clusterboot(data = d1, 
+                      clustermethod = hclustCBI,
+                      method="ward.D2",
+                      seed = 333,
+                      k=3,
+                      B = 100 # number of resamples/bootstraps
+)
+print.clboot(cboot3) # look at all outputs
 
-# visualize:
-ggclust3 <- fviz_cluster(list(data=d1, cluster=hc3_grps))
-ggclust3 + theme_classic() +
-  labs(title = "Clusters for CA Thermal Regimes (k=5)")
-ggsave("output/figures/pc_diana_k3.png", width = 8, height = 6, units="in", dpi=300)
-
-
-# TangleGram --------------------------------------------------------------
-
-library(dendextend)
-
-# Create two dendrograms
-dend1 <- as.dendrogram (hc2)
-dend2 <- as.dendrogram (hc3)
-
-# no idea how this is useful but it's cool! :)
-tanglegram(dend1, dend2)
+# try with hclust k=6
+cboot6 <- clusterboot(data = d1, 
+                      clustermethod = hclustCBI,
+                      method="ward.D2",
+                      seed = 333,
+                      k=6,
+                      B = 100 # number of resamples/bootstraps
+)
+print.clboot(cboot6) # look at all outputs
 
 
-# HCLUST: Stats  -----------------------------------------------------------
+# still would go with k=5
+cboot5_grps <- cboot5$result$partition # vector of cluster labels
+table(cboot5_grps) # table of how many per group
+cboot5$bootmean # cluster stabilities (Jaccard Scores)
+# so group 3 is highly stable, then group 1 and then group 2
+
+# Save out groups based on bootstrapping
+# make into a dataframe
+cboot5_df <- as.data.frame(cboot5_grps) %>% 
+  rename(k_5=cboot5_grps) %>% 
+  mutate(station_id = rownames(.)) %>% 
+  select(station_id, k_5)
+
+
+# cluster stats
+cluster.stats(d1, clustering = cboot5_grps)
+
+# 04. Additional Stats to pick K  ----------------------------------------
 
 # calculating the Calinski-Harabasz Index
 
@@ -328,77 +199,120 @@ CHCriterion <- function( data, kmax, clustermethod, ...  )
                 plot = plot ) )
 }
 
-
 # APPLY to our Data using the ward method
 kcriteria <- CHCriterion(data = ann_metrics_s, kmax=8,
                          clustermethod = "hclust", method="ward.D2")
 
 kcriteria$data
 kcriteria$plot
-ggsave("output/figures/wss_and_ch_index_for_clusters.png", width = 10, height = 8, units="in", dpi = 300)
-# based on this, looks like 5...
+#ggsave("output/figures/wss_and_ch_index_for_clusters.png", width = 10, height = 8, units="in", dpi = 300)
 
-# HCLUST: {fpc} -----------------------------------------------------------
+## Additional Stats
+# nice overview here: https://bradleyboehmke.github.io/HOML/hierarchical.html#determining-optimal-clusters
 
-library(fpc)
+# WSS::elbow methods: define clusters such that the total within-cluster variation is minimized, so choose a k that minimizes the total within-cluster sum of squares (a measure of the compactness of the clustering).
+p1 <- fviz_nbclust(ann_metrics_s, FUN = hcut, method = "wss", 
+                   k.max = 8) + 
+  #geom_vline(xintercept = 5, linetype=3, col="gray") +
+  ggtitle("WSS/Elbow method")
 
-# set the desired number of clusters
-k_try <- 5
+# Silhouette method (Rousseeuw 1987)
+p2 <- fviz_nbclust(ann_metrics_s, FUN = hcut, method = "silhouette", 
+                   k.max = 8) +
+  ggtitle("Silhouette method")
 
-# try 
-cboot5 <- clusterboot(data = d1, 
-                        clustermethod = hclustCBI,
-                        method="ward.D2", 
-                        k=k_try,
-                        B = 1000 # number of resamples/bootstraps
-                        )
-
-cboot5_grps <- cboot5$result$partition # vector of cluster labels
-print.clboot(cboot5) # look at all outputs
-table(cboot5_grps) # table of how many per group
-cboot5$bootmean # cluster stabilities (Jaccard Scores)
-# so group 3 is highly stable, then group 4 and then group 1. Group 5 is less so
+# Display plots side by side
+(p4 <-cowplot::plot_grid(p1, p2, nrow = 1, labels = c("B","C")))
 
 
-# Save out groups based on bootstrapping
-# make into a dataframe
-cboot5_df <- as.data.frame(cboot5_grps) %>% 
-  rename(k_5=cboot5_grps) %>% 
-  mutate(station_id = rownames(.)) %>% 
-  select(station_id, k_5)
+# HCLUST: {hclust} ------------------------------------------------------------------
+
+## Agglomerative Nesting Clustering
+
+# Hierarchical clustering using Complete Linkage
+hc1 <- hclust(d1, method = "ward.D2")
+
+# PLOT Dendrograms
+
+# k=5
+plot(hc1, cex = 0.6, hang = -1)
+# see how many clusters with k = 5 (totally arbitrary)
+rect.hclust(hc1, k = 5, border = viridis::viridis(5))
+
+# k=6
+plot(hc1, cex = 0.6, hang = -1)
+rect.hclust(hc1, k = 6, border = viridis::viridis(6))
+
+# k=3
+plot(hc1, cex = 0.6, hang = -1, main="Dendrogram {hclust}: Wards")
+rect.hclust(hc1, k = 3, border = viridis::viridis(3))
+
+# HCLUST: {agnes} -------------------------------------------------------
+
+# Agglomerative Nesting Clustering
+
+# use agnes to get agglomerative coefficient
+# closer to 1 is stronger clusterings
+hc2 <- agnes(d1, method = "ward")
+hc2$ac
+
+# try diff methods to assess
+m <- c( "average", "single", "complete", "ward")
+names(m) <- c( "average", "single", "complete", "ward")
+
+# function to compute coefficient
+ac <- function(x) {
+  agnes(d1, method = x)$ac
+}
+
+# so wards is still best option here...
+map_dbl(m, ac)
+
+# plot and add clusters
+pltree(hc2, cex = 0.6, hang = -1, main = "Dendrogram {agnes}: Wards, k=5") 
+rect.hclust(hc2, k = 5, border = viridis::viridis(5))
+
+# try with k=3
+pltree(hc2, cex = 0.6, hang = -1, main = "Dendrogram {agnes}: Wardsm k=6") 
+rect.hclust(hc2, k = 6, border = viridis::viridis(3))
+
+# try with k=3
+pltree(hc2, cex = 0.6, hang = -1, main = "Dendrogram {agnes}: Wards, k=3") 
+rect.hclust(hc2, k = 3, border = viridis::viridis(3))
+
+# the groups out
+hc2_grps_k6 <- cutree(hc2, k=6) # try k=5
+table(hc2_grps_k6)
+hc2_grps_k5 <- cutree(hc2, k=5) # try k=5
+hc2_grps_k3 <- cutree(hc2, k=3) # try k=3
+
+# bind together and save
+agnes_k_groups <- tibble(site_id = ann_metrics$station_id, k_3=hc2_grps_k3, k_5=hc2_grps_k5, k_6=hc2_grps_k6)
+save(agnes_k_groups, file="output/models/agnes_k_groups_final.rda")
+
+# k=6
+ggclust2_k6 <- fviz_cluster(list(data=d1, cluster=hc2_grps_k6))
+ggclust2_k6 + theme_classic() +
+  labs(title = "Clusters for CA Thermal Regimes (k=6)")
+ggsave("output/figures/pc_agnes_k6.png", width = 8, height = 6, units="in", dpi=300)
+
+# k=5
+ggclust2_k5 <- fviz_cluster(list(data=d1, cluster=hc2_grps_k5))
+ggclust2_k5 + theme_classic() +
+  labs(title = "Clusters for CA Thermal Regimes (k=5)")
+ggsave("output/figures/pc_agnes_k5.png", width = 8, height = 6, units="in", dpi=300)
+
+# plot k=3
+ggclust2_k3 <- fviz_cluster(list(data=d1, cluster=hc2_grps_k3))
+ggclust2_k3 + theme_classic() +
+  labs(title = "Clusters for CA Thermal Regimes (k=3)")
+ggsave("output/figures/pc_agnes_k3.png", width = 8, height = 6, units="in", dpi=300)
 
 
-## Now try with 3
-k_try <- 3
 
-# try 
-cboot3 <- clusterboot(data = d1, 
-                      clustermethod = hclustCBI,
-                      method="ward.D2", 
-                      k=k_try,
-                      B = 1000 # number of resamples/bootstraps
-)
+# Save out Group Bootstrap k's --------------------------------------------
 
-cboot3_grps <- cboot3$result$partition # vector of cluster labels
-print.clboot(cboot3) # look at Jaccard Scores here
-table(cboot3_grps)
-cboot3$bootmean # highly stable
+# save # output/models/agnes_k_groups_final.rda
+write_csv(cboot5_df, path="output/models/classification_group_results.csv")
 
-# make df of groups
-cboot3_df <- as.data.frame(cboot3_grps) %>% 
-  rename(k_3=cboot3_grps) %>% 
-  mutate(station_id = rownames(.)) %>% 
-  select(station_id, k_3)
-
-# cluster stats
-cluster.stats(d1, clustering = cboot5_grps)
-
-
-# Bind and Save out Groups ------------------------------------------------
-
-class_groups <- left_join(cboot3_df, cboot5_df)
-
-# save
-write_csv(class_groups, path="output/models/classification_group_results.csv")
-
-save(class_groups, file = "output/models/classification_group_results.rda")
+save(cboot5_df, file = "output/models/classification_group_results.rda")
