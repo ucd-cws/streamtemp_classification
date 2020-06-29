6b16289bdfe68cfb6a2c3adbf4fe0317c7431960
Ann Willis <awillis@ucdavis.edu>
2020-06-29 11:25:32 -0700

    Developed code to explore thermal regime results

--- code/10d_cluster_metric_details.R
+++ code/10d_cluster_metric_details.R
@@ -44,4 +44,46 @@ variable_warm <- thermal_regime_parameters %>%
 
 variable_warm$date <- format(as.Date(variable_warm$mean_DOWY, origin = "2018-10-01"), "%m-%d")
 
+variable_warm <- thermal_regime_parameters %>% 
+  filter(k_5 == 3)
 
+variable_warm$date <- format(as.Date(variable_warm$DOWY, origin = "2018-10-01"), "%m-%d")
+
+max(variable_warm$ann_max)
+min(variable_warm$ann_max)
+
+max(variable_warm$ann_mean)
+min(variable_warm$ann_mean)
+
+DOWY_max <- max(variable_warm$DOWY)
+DOWY_min <- min(variable_warm$DOWY)
+
+format(as.Date(DOWY_max, origin = "2018-10-01"), "%m-%d")
+format(as.Date(DOWY_min, origin = "2018-10-01"), "%m-%d")
+
+summary(variable_warm)
+
+format(as.Date(298, origin = "2018-10-01"), "%m-%d")
+
+variable_warm[variable_warm$DOWY == DOWY_max, 1:3]
+
+# stable cool
+stable_cool <- thermal_regime_parameters %>% 
+  filter(k_5 == 4)
+
+stable_cool$date <- format(as.Date(stable_cool$DOWY, origin = "2018-10-01"), "%m-%d")
+
+summary(stable_cool)
+
+format(as.Date(309, origin = "2018-10-01"), "%m-%d")
+format(as.Date(283, origin = "2018-10-01"), "%m-%d")
+
+# variable_cool
+variable_cool <- thermal_regime_parameters %>% 
+  filter(k_5 == 2)
+
+summary(variable_cool)
+
+format(as.Date(305, origin = "2018-10-01"), "%m-%d")
+format(as.Date(min(variable_cool$DOWY), origin = "2018-10-01"), "%m-%d")
+format(as.Date(max(variable_cool$DOWY), origin = "2018-10-01"), "%m-%d")
