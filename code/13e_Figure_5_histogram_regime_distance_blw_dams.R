a5761ddc7eb59185059094d84cc9b6b677cfada5
Ann Willis <awillis@ucdavis.edu>
2020-07-23 09:53:57 -0700

    Finished writing code to make Figure 5: distance to dam stacked histogram. Renamed 13e

--- code/13e_Figure_5_histogram_regime_distance_blw_dams.R
+++ code/13e_Figure_5_histogram_regime_distance_blw_dams.R
@@ -17,17 +17,17 @@ library(tidyverse)
 
 load("output/12_data_k_centdist_damdist.rda")
 
-# filter out to just reg warm/reg cool; this is code copied from 12_get_dam_distances
-regimes_2_3 <- data_k_dist %>% 
-  st_drop_geometry() %>% 
-  filter(k5_names %in% c("2-variable warm", "3-stable cool")) %>%
-  group_by(k_5, k5_names) 
+# filter out to just reg warm/reg cool 
 
-ggplot(regimes_2_3) + 
-  geom_histogram(aes(x=cum_len_km, fill=color), 
+# this is code copied from 12_get_dam_distances
+data_k_dist %>% st_drop_geometry() %>% 
+  filter(k5_names %in% c("2-variable warm", "3-stable cool")) %>%
+  group_by(k_5, k5_names) %>% 
+  ggplot() + geom_histogram(aes(x=cum_len_km, fill=color), 
                             binwidth = 3, bins = 50) +
+  facet_grid(k5_names~.) + 
   labs(x= "distance below dam (km)") +
-  scale_fill_manual(name="thermal regime",values = c("#FF7F00","#984EA3"),labels=c("variable warm", "stable cool")) +
   theme_clean()
 
+ggsave("output/figures/Figure_5_dist_to_dam.png", width = 8, height = 6, units = "in", dpi = 600)
 
